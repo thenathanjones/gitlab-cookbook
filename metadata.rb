996e97f78344e76ae71a8b6fdcf9bcc7292a72f8
Nathan Jones <thenathanjones@gmail.com>
2015-05-12 14:41:45 +1200

    Initial skeleton for Gitlab cookbook

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'gitlab'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures gitlab'
+long_description 'Installs/Configures gitlab'
+version          '0.1.0'
